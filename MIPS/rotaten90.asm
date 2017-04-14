494dc9878d357a78def38650913cea9287aa9312
Peperzastey <przemyslaw.poljanski@gmail.com>
2017-04-14 14:24:54 +0200

    Add (4k+1)*90 rotations impl
    Add also some mess in code and register assignment. Needs tidying up.

--- MIPS/rotaten90.asm
+++ MIPS/rotaten90.asm
@@ -30,16 +30,17 @@ readcom:	.asciiz "\nRead: "
 wrotecom:	.asciiz "\nWrote: "
 rowsize:	.asciiz "\nRowsize: "
 pix_number:	.asciiz "\nPixel number: "
+allocadr:	.asciiz "\nAllocated at addr: "
 		.align	2
 BM:		.ascii	"BM"
 
 # Symbol Table:
 # $s7 - input file's descriptor	then output	!
 # $s6 - size of pixel array (in bytes)		! = $s3 - (18 + $s1)
-# $s5 - temp: how many rotations		!
-# $s4 - 2 least significant bytes of input n, then output buffer
-# $s3 - file size				!
-# $s2 - pixel array start (offset)		!
+# $s5 - temp: how many rotations		, then new padding size (in bytes)
+# $s4 - 2 least significant bytes of input n	, then output buffer(for 180* only)
+# $s3 - file size				, then new row size (for 90* ,270* rotations)
+# $s2 - pixel array start (offset)		, then output buffer(for 90*, 270*)
 # $s1 - DIB header size				!
 # Temporary saved:
 # $t9 - bitmap's width in pixels		!!
@@ -235,7 +236,7 @@ input:		li	$v0, 4
 		la	$a0, in_n
 		syscall
 		####
-		li	$s5, -1
+		li	$s5, 9
 		####
 		li	$v0, 4
 		la	$a0, fed
@@ -262,9 +263,16 @@ write:	# open
 		
 chck_n:		li	$t0, 0x0003		# divisibility by 4 : 2ls bytes = 0
 		and	$s4, $s5, $t0
+		
+	# calculate padding of rows in pixel array of input file [TODO][?][MAYBE]
+		
 		beq	$s4, 0, same_hdr	# write without changes [TODO]
 		beq	$s4, 2, same_hdr
 		#beq	$t0, 3, rotneg
+		
+		
+#====================================================================================================[=]	
+
 		
 	# calculate new header fields for 90* and 270* rotations (they are the same)
 	# swap width and height (in pixels)
@@ -275,29 +283,32 @@ chck_n:		li	$t0, 0x0003		# divisibility by 4 : 2ls bytes = 0
 		# prerequisite: 24bit/px
 		# $t9 - new width in pixels
 		# $t8 - new height in pixels
-		# $t7 - bits/px
+		# $t7 - bits/px -> padding (new) -> padding (old)? [?]
 		# $t6 - input pixel array
+		
+		# $t3 - size of new row (in bytes)
 		srl	$t2, $t7, 3		# bytes/px (divide by 8)
 		multu	$t2, $t9
 		mflo	$t3			# size of row, without padding
+		move	$s2, $t3	# stop condition for inner loop
 		and	$t5, $t3, 0x0003	# row_size mod 4 (bytes)
 		move	$t7, $zero		# [TODO][temp] padding
 		beqz	$t5, qtrrot_nopad	# no padding
 		li	$t4, 4
-		subu	$t5, $t4, $t5		# padding
-		move	$t7, $t5		# [TODO][temp] padding
+		subu	$t5, $t4, $t5		# padding (new)
+		move	$t7, $t5		# [TODO][temp] padding (new)(in bytes)
 		addu	$t3, $t3, $t5		# size of row (in bytes)
-qtrrot_nopad:	#li	$v0, 9			# allocate buffer for 1 row
-		#move	$a0, $t3
-		#syscall
-		#move	$s4, $v0
+qtrrot_nopad:	li	$v0, 9			# allocate buffer for 1 row
+		move	$a0, $t3
+		syscall
+		move	$s3, $v0
 			# debug
-			#li	$v0, 4
-			#la	$a0, readcom
-			#syscall
-			#li	$v0, 1
-			#move	$a0, $s4
-			#syscall
+			li	$v0, 4
+			la	$a0, allocadr
+			syscall
+			li	$v0, 1
+			move	$a0, $s3
+			syscall
 			# _debug
 	# calculate new BMP data size (new pixel array size) (in bytes)
 		# $t3 - size of row (in bytes)
@@ -362,8 +373,42 @@ qtrrot_nopad:	#li	$v0, 9			# allocate buffer for 1 row
 			syscall
 			li	$v0, 1
 			move	$a0, $t1
-			syscall	
-		beq	$s4, 3, negqtrrot	
+			syscall
+			
+	# calculate size of old rows (in bytes)
+		multu	$t2, $t8		# mult by old width
+		mflo	$t7			# size of old row, without padding
+		and	$t5, $t7, 0x0003	# row_size mod 4 (bytes)
+		#move	$t2, $zero		# [TODO][temp] padding
+		beqz	$t5, qtrrot_nopadold	# no padding
+		li	$t4, 4
+		subu	$t5, $t4, $t5		# padding (old)
+		#move	$t2, $t5		# [TODO][temp] padding (old)(in bytes)
+		addu	$t7, $t7, $t5		# size of old row (in bytes)
+qtrrot_nopadold:	
+			
+		beq	$s4, 3, negqtrrot
+		
+# prerequisite: 24bit/px
+# $s7 - output fd					!
+# $s6 - size of pixel array			?	<used in same_hdr>
+# $s5 - 								{was user's input n}
+# $s4 - ;output buffer for 1 new row		{was 2 least significant bytes of input n}
+# $s3 - //let: output buffer
+# $s2 - stop cond for inner loop (buf + new row size without padding) ABSOLUTE!
+# $s1 - DIB header size				?
+
+# $t9 - new width in pixels
+# $t8 - new height in pixels
+# $t7 - old row size
+# $t6 - input pixel array
+# $t5 - ABS pos output					old: old padding
+# $t4 -	ABS pos input	
+# $t3 - size of new row (in bytes)	
+# $t2 - pixel size in bytes
+# $t1 - POM, to copy form input to output buff
+# $t0 - start pos of inner loop (last pixel first row);stop condition of inner loop
+
 qtrrot:	
 	#rot90(pos) -- mask-and result=1
 	# pixel array : 90*	
@@ -371,10 +416,6 @@ qtrrot:
 		la	$a0, r90
 		syscall		
 #post qtrrot_nopad:
-		# NOTE: for now it's just a copy of halfrot algorithm !!
-		# $t3 - size of row (in bytes)
-		# $t2 - bytes/px
-		# $s4 - output buffer (for 1 row)
 			# debug
 			li	$v0, 4
 			la	$a0, rowsize
@@ -383,44 +424,47 @@ qtrrot:
 			move	$a0, $t3
 			syscall
 			# _debug
-		addiu	$t0, $t8, -1
-		multu	$t0, $t3
-		mflo	$t0			# start byte of last row
-		addiu	$t1, $t9, -1
+			
+		
+		#;addiu	$t0, $t8, -1
+		#;multu	$t0, $t3
+		#;mflo	$t0	
+		addiu	$t1, $t8, -1
 		multu	$t1, $t2
-		mflo	$t1			# byte offset to last pixel in row
-		addu	$t4, $t0, $t1
-		# $t4 - current position (byte)	in pixel array
+		mflo	$t1			# last pixel in first row (old)
 			# debug
 			li	$v0, 4
-			la	$a0, pix_number
+			la	$a0, pix_number	# RELATIVE
 			syscall
 			li	$v0, 1
-			move	$a0, $t4
+			move	$a0, $t1
 			syscall
 			# _debug
-		addu	$t4, $t6, $t4		# ABSOLUTE	-- input
-			# move	$t5, $zero	
-		addu	$t5, $s4, $zero		# ABSOLUTE	-- output
-		# $t5 - current position (byte) in output buffer ABSOLUTE
-		addu	$t0, $t6, $t0
-		# $t0 - start byte of current row ABSOLUTE
-		# write 2ms bytes of pixel to buffer
+		addu	$t4, $t6, $t1		# ABSOLUTE -- input
+		move	$t0, $t4		# start pos in input
+				
+		addu	$t5, $s3, $zero		# ABSOLUTE -- output
+		addu	$s2, $s3, $s2		# stop condition for inner loop
+		#;addu	$t0, $t6, $t0
+		#;# $t0 - start byte of current row ABSOLUTE
+
 r90_fillbuf:	lbu	$t1, 0($t4)
 		sb	$t1, 0($t5)
 		lbu	$t1, 1($t4)
 		sb	$t1, 1($t5)
 		lbu	$t1, 2($t4)
 		sb	$t1, 2($t5)
-		addiu	$t4, $t4, -3
+		
+		addu	$t4, $t4, $t7		# move on to next row (the same column)
 		addiu	$t5, $t5, +3
-		bgeu	$t4, $t0, r90_fillbuf	# not end of row yet
-		# reached end of row
+		bltu	$t5, $s2, r90_fillbuf	# not end of output row yet
+		
+		# reached end of column
 	# [TODO]	...			# add necessary padding (zeros)
 		# write buffer to output file
 		li	$v0, 15
 		move	$a0, $s7
-		move	$a1, $s4		# output buffer with 1 row
+		move	$a1, $s3		# output buffer with 1 row
 		move	$a2, $t3
 		syscall
 			# check
@@ -431,13 +475,15 @@ r90_fillbuf:	lbu	$t1, 0($t4)
 			#li	$v0, 1
 			#move	$a0, $v1
 			#syscall
-		# $t7 - padding (in bytes)
-		# move to next row (upwards)
-		subu	$t0, $t0, $t3		# start byte of the next row
-		addu	$t5, $s4, $zero		# reset position in buffer ABSOLUTE
-		subu	$t4, $t4, $t7
-		subu	$t4, $t4, $t2		# set position in pixel array to next pixel to load
-		bgeu	$t0, $t6, r90_fillbuf	# branch if there are more rows left to rotate			
+		#;# $t7 - padding (in bytes)
+		# move to next column (towards left)
+		#;subu	$t0, $t0, $t3		# start byte of the next row
+		addu	$t5, $s3, $zero		# reset position in buffer ABSOLUTE
+		#;subu	$t4, $t4, $t7
+		#;subu	$t4, $t4, $t2		# set position in pixel array to next pixel to load
+		addiu	$t4, $t0, -3
+		addiu	$t0, $t0, -3		#;move $t0, $t4
+		bgeu	$t4, $t6, r90_fillbuf	# branch if there are more columns left to rotate			
 		b close
 	# or branch to done
 negqtrrot:	
@@ -500,7 +546,7 @@ halfrot:
 		beqz	$t5, halfrot_nopad	# no padding
 		li	$t4, 4
 		subu	$t5, $t4, $t5		# padding
-		move	$t7, $t5		# [TODO][temp] padding
+		move	$t7, $t5		# [TODO][temp] padding (in bytes)
 		addu	$t3, $t3, $t5		# size of row (in bytes)
 halfrot_nopad:	li	$v0, 9			# allocate buffer for 1 row
 		move	$a0, $t3
